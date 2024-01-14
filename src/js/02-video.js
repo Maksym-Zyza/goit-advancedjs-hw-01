de7f79ef2641a8ed1cf753048be657a28556fb75
Maksym Zyza <mmziza88@gmail.com>
2024-01-14 17:41:09 +0200

    added task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const KEY_SECONDS = 'videoplayer-current-time';
+const UPDATE_TIME = 1000;
+
+const savePlayback = () => {
+  player
+    .getCurrentTime()
+    .then(seconds => {
+      localStorage.setItem(KEY_SECONDS, seconds);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+const throttlePlayback = throttle(savePlayback, UPDATE_TIME);
+player.on('timeupdate', () => {
+  throttlePlayback();
+});
+
+const getPlayback = () => {
+  const savedTime = localStorage.getItem(KEY_SECONDS) ?? 0;
+  player
+    .setCurrentTime(savedTime)
+    .then(() => {
+      // player.play();
+    })
+    .catch(err => {
+      console.error('Error playback:', err);
+    });
+};
+
+getPlayback();
